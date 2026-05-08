a86579c788c813d1f237c9172be343f6a5525ca8
0younge <s45628439@gmail.com>
2026-05-08 17:31:39 +0900

    first commit

---
+++ src/main/java/com/example/secureboardexample/SecureboardexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureboardexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureboardexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureboardexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secureboardexample/SecureboardexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureboardexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureboardexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
